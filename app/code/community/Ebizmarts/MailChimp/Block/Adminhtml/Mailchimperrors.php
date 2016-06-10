ff770ad3489184f2dbfe3c37eeba9f0eea1051d6
gonzalo <gonzalo@ebizmarts.com>
2016-06-10 14:56:57 -0300

    Add errors grid

---
+++ app/code/community/Ebizmarts/MailChimp/Block/Adminhtml/Mailchimperrors.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * mc-magento Magento Component
+ *
+ * @category Ebizmarts
+ * @package mc-magento
+ * @author Ebizmarts Team <info@ebizmarts.com>
+ * @copyright Ebizmarts (http://ebizmarts.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @date: 6/10/16 1:42 PM
+ * @file: Mailchimperrors.php
+ */
+class Ebizmarts_Mailchimp_Block_Adminhtml_Mailchimperrors extends Mage_Adminhtml_Block_Widget_Grid_Container
+{
+    public function __construct()
+    {
+        // The blockGroup must match the first half of how we call the block, and controller matches the second half
+        // ie. foo_bar/adminhtml_baz
+        $this->_blockGroup = 'mailchimp';
+        $this->_controller = 'adminhtml_mailchimperrors';
+        $this->_headerText = $this->__('Mailchimp errors');
+
+        parent::__construct();
+        $this->removeButton('add');
+
+    }
+
+}
