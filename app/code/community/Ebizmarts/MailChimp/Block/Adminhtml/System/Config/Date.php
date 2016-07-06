9bdefded6cbe74a5f1b236ece0420435ca82d8c3
gonzalo <gonzalo@ebizmarts.com>
2016-07-06 15:11:09 -0300

    add first date for abandoned cart

---
+++ app/code/community/Ebizmarts/MailChimp/Block/Adminhtml/System/Config/Date.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * mc-magento Magento Component
+ *
+ * @category Ebizmarts
+ * @package mc-magento
+ * @author Ebizmarts Team <info@ebizmarts.com>
+ * @copyright Ebizmarts (http://ebizmarts.com)
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @date: 7/5/16 2:19 PM
+ * @file: Date.php
+ */
+class Ebizmarts_MailChimp_Block_Adminhtml_System_Config_Date extends Mage_Adminhtml_Block_System_Config_Form_Field
+{
+    public function render(Varien_Data_Form_Element_Abstract $element)
+    {
+        $element->setFormat(Varien_Date::DATE_INTERNAL_FORMAT);
+        $element->setImage($this->getSkinUrl('images/grid-cal.gif'));
+        return parent::render($element);
+    }
+}
