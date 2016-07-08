8f8f3048ec7310c40f1ce7c48c24873c78492c33
gonzalo <gonzalo@ebizmarts.com>
2016-07-08 16:41:02 -0300

    delete mailchimp data from quote_item table

--- app/code/community/Ebizmarts/MailChimp/sql/mailchimp_setup/mysql4-upgrade-1.0.4-1.0.5.php
+++ app/code/community/Ebizmarts/MailChimp/sql/mailchimp_setup/mysql4-upgrade-1.0.4-1.0.5.php
@@ -18,8 +18,6 @@ $installer->startSetup();
 
 try {
     $installer->run("
- ALTER TABLE `{$this->getTable('sales_flat_quote_item')}` ADD column `mailchimp_sync_delta` datetime NOT NULL;
- ALTER TABLE `{$this->getTable('sales_flat_quote_item')}` ADD column `mailchimp_sync_error` VARCHAR(255) NOT NULL;
  ALTER TABLE `{$this->getTable('sales_flat_quote')}` ADD column `mailchimp_sync_delta` datetime NOT NULL;
  ALTER TABLE `{$this->getTable('sales_flat_quote')}` ADD column `mailchimp_deleted` INT(1) NOT NULL DEFAULT 0;
  ALTER TABLE `{$this->getTable('sales_flat_quote')}` ADD column `mailchimp_token` VARCHAR(255) NOT NULL DEFAULT '';
