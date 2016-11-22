4450ed70a41b89d7cce9f9e19bae7f1ac19f2d9c
gonzalo <gonzalo@ebizmarts.com>
2016-11-22 10:01:11 -0300

    closes #101

--- app/code/community/Ebizmarts/MailChimp/Model/Observer.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Observer.php
@@ -282,7 +282,7 @@ class Ebizmarts_MailChimp_Model_Observer
                 'filter' => false,
                 'renderer' => 'mailchimp/adminhtml_sales_order_grid_renderer_mailchimp',
                 'sortable' => false,
-                'width' => 170
+                'width' => 70
             ), 'created_at'
             );
         }
