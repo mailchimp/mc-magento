02a100a78c315622935615939f3aaf48a0d00798
Santiago <santiago@ebizmarts.com>
2017-04-27 15:50:47 -0300

    Offset domain error solved. closes #186

--- app/code/community/Ebizmarts/MailChimp/Model/Email/Template.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Email/Template.php
@@ -90,10 +90,11 @@ class Ebizmarts_MailChimp_Model_Email_Template extends Mage_Core_Model_Email_Tem
         $senderExists = false;
         foreach ($mandrillSenders as $sender)
         {
-            $emailArray = explode('@', $email['from_email']);
-            if(count($emailArray) > 1 && $emailArray[1] == $sender['domain'])
-            {
-                $senderExists = true;
+            if (isset($sender['domain'])) {
+                $emailArray = explode('@', $email['from_email']);
+                if (count($emailArray) > 1 && $emailArray[1] == $sender['domain']) {
+                    $senderExists = true;
+                }
             }
         }
 
