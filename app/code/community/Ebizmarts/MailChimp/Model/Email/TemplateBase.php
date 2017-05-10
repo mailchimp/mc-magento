087aec7983f4c9474da382ff6df3b84e0b1b5c0b
Kim Sondrup <kim@sondrup.xyz>
2017-05-10 18:05:00 +0200

    Follow MEQP1 rule "Only one class is allowed in a file"

--- app/code/community/Ebizmarts/MailChimp/Model/Email/TemplateBase.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Email/TemplateBase.php
@@ -2,15 +2,11 @@
 if (Mage::helper('core')->isModuleEnabled('Aschroder_SMTPPro')
     && class_exists('Aschroder_SMTPPro_Model_Email_Template')
 ) {
-    class Ebizmarts_MailChimp_Model_Email_TemplateBase extends Aschroder_SMTPPro_Model_Email_Template
-    {
-    }
+    class_alias('Aschroder_SMTPPro_Model_Email_Template', 'Ebizmarts_MailChimp_Model_Email_TemplateBase');
 } elseif (Mage::helper('core')->isModuleEnabled('Aschroder_Email')
     && class_exists('Aschroder_Email_Model_Email_Template')
 ) {
-    class Ebizmarts_MailChimp_Model_Email_TemplateBase extends Aschroder_Email_Model_Email_Template
-    {
-    }
+    class_alias('Aschroder_Email_Model_Email_Template', 'Ebizmarts_MailChimp_Model_Email_TemplateBase');
 } else {
     class Ebizmarts_MailChimp_Model_Email_TemplateBase extends Mage_Core_Model_Email_Template
     {
