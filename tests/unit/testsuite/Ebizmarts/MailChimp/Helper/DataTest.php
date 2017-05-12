b91336a2f215e22ec30f258e175ff09a1938d5b7
centerax <pablo@ebizmarts.com>
2017-05-12 17:02:38 +0200

    testCustomMergeFieldAlreadyExists

--- tests/unit/testsuite/Ebizmarts/MailChimp/Helper/DataTest.php
+++ tests/unit/testsuite/Ebizmarts/MailChimp/Helper/DataTest.php
@@ -22,4 +22,21 @@ class Ebizmarts_MailChimp_Helper_DataTest extends PHPUnit_Framework_TestCase
         $this->assertNull($helperMock->getLastDateOfPurchase("john@example.com"));
     }
 
+    public function testCustomMergeFieldAlreadyExists()
+    {
+        /** @var \Ebizmarts_MailChimp_Helper_Data $helperMock */
+        $helperMock = $this->getMockBuilder(Ebizmarts_MailChimp_Helper_Data::class)
+            ->disableOriginalConstructor()
+            ->setMethods(array('getCustomMergeFields'))
+            ->getMock();
+
+        $helperMock->expects($this->once())->method('getCustomMergeFields')->with(0, "store")
+        ->willReturn(array(
+            array(
+                "value" => "FNAME"
+            )
+        ));
+
+        $this->assertTrue($helperMock->customMergeFieldAlreadyExists("FNAME", 0, "store"));
+    }
 }
