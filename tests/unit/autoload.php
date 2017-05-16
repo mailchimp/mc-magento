fb8679023f04aaf6355da97b453741e18c3dfc76
centerax <pablo@ebizmarts.com>
2017-05-16 11:27:27 +0200

    travis

--- tests/unit/autoload.php
+++ tests/unit/autoload.php
@@ -3,6 +3,7 @@
 ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR
     . dirname(__FILE__) . '/../../../../app' . PATH_SEPARATOR
     . dirname(__FILE__) . '/../../app' . PATH_SEPARATOR
+    . dirname(__FILE__) . '/../../../app' . PATH_SEPARATOR
     . dirname(__FILE__));
 
 //Set custom memory limit
