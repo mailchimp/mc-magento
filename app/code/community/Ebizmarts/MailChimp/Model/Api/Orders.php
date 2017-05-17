48c509bf518edaece67a9da15ca15f6fa1b5353b
Pablo <pablos.benitez@gmail.com>
2017-05-17 16:00:20 +0200

    Update Orders.php

--- app/code/community/Ebizmarts/MailChimp/Model/Api/Orders.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Api/Orders.php
@@ -186,7 +186,7 @@ class Ebizmarts_MailChimp_Model_Api_Orders
 
         $data['currency_code'] = $order->getOrderCurrencyCode();
         $data['order_total'] = $order->getGrandTotal();
-        $data['tax_total'] = $order->getTaxAmount()==null?0:$order->getTaxAmount();
+        $data['tax_total'] = $this->returnZeroIfNull($this->getTaxAmount());
         $data['discount_total'] = abs($order->getDiscountAmount());
         $data['shipping_total'] = $order->getShippingAmount()==null?0:$order->getShippingAmount();
         $statusArray = $this->_getMailChimpStatus($order);
@@ -435,6 +435,15 @@ class Ebizmarts_MailChimp_Model_Api_Orders
         return $jsonData;
     }
 
+    protected function returnZeroIfNull($value)
+    {
+        $returnValue = $value;
+        if ($value === null) {
+            $returnValue = 0;
+        }
+        return $returnValue;
+    }
+    
     protected function _getMailChimpStatus($order)
     {
         $mailChimpFinancialStatus = null;
