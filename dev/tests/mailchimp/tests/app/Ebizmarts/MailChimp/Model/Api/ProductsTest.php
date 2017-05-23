480d4a5cb14ee69ee4a16ef623c5b305d5e7254b
centerax <pablo@ebizmarts.com>
2017-05-23 14:34:17 -0300

    tests

--- dev/tests/mailchimp/tests/app/Ebizmarts/MailChimp/Model/Api/ProductsTest.php
+++ dev/tests/mailchimp/tests/app/Ebizmarts/MailChimp/Model/Api/ProductsTest.php
@@ -2,26 +2,40 @@
 
 class Ebizmarts_MailChimp_Model_Api_ProductsTest extends PHPUnit_Framework_TestCase
 {
+    private $productsApiMock;
+
     public function setUp()
     {
         Mage::app('default');
+
+        /** @var Ebizmarts_MailChimp_Model_Api_Products $apiProductsMock */
+        $this->productsApiMock = $this->getMockBuilder(Ebizmarts_MailChimp_Model_Api_Products::class);
     }
 
     public function testCreateBatchJson()
     {
-        /** @var Ebizmarts_MailChimp_Model_Api_Products $apiProductsMock */
-        $apiProductsMock = $this->getMockBuilder(Ebizmarts_MailChimp_Model_Api_Products::class)
-            ->disableOriginalConstructor()
-            ->setMethods(array('sendModifiedProduct', 'generateBatchId', 'makeProductsNotSentCollection'))
+        $this->productsApiMock = $this->productsApiMock->setMethods(array('generateBatchId'))
             ->getMock();
 
-        $apiProductsMock->expects($this->once())->method('generateBatchId')->with(0)
+        $this->productsApiMock->expects($this->once())->method('generateBatchId')->with(0)
             ->willReturn('storeid-0_PRO_2017-05-18-14-45-54-38849500');
-        $apiProductsMock->expects($this->never())->method('buildProductDataRemoval');
+        $this->productsApiMock->expects($this->never())->method('buildProductDataRemoval');
 
-        $apiProductsMock->expects($this->once())->method('makeProductsNotSentCollection')->with('dasds231231312', 0)
-        ->willReturn(new Varien_Object());
+//        $this->productsApiMock->expects($this->once())->method('makeProductsNotSentCollection')->with('dasds231231312', 0)
+//        ->willReturn(new Varien_Object());
 
-        $apiProductsMock->createBatchJson('dasds231231312', 0);
+        $this->productsApiMock->createBatchJson('dasds231231312', 0);
+    }
+
+    public function testMakeProductsNotSentCollection()
+    {
+        $this->productsApiMock = $this->productsApiMock->setMethods(array('sendModifiedProduct', 'generateBatchId'))
+            ->getMock();
+
+//        $product->getProductUrl()
+
+        $collection = $this->productsApiMock->makeProductsNotSentCollection('dasds123321', 0);
+
+        $this->assertEquals("select", (string)$collection->getSelect());
     }
 }
