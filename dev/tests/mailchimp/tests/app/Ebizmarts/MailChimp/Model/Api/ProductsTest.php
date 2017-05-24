3da3362f30a097536fde803df9ea7d55468cecf1
centerax <pablo@ebizmarts.com>
2017-05-24 09:38:22 -0300

    tests update

--- dev/tests/mailchimp/tests/app/Ebizmarts/MailChimp/Model/Api/ProductsTest.php
+++ dev/tests/mailchimp/tests/app/Ebizmarts/MailChimp/Model/Api/ProductsTest.php
@@ -12,30 +12,62 @@ class Ebizmarts_MailChimp_Model_Api_ProductsTest extends PHPUnit_Framework_TestC
         $this->productsApiMock = $this->getMockBuilder(Ebizmarts_MailChimp_Model_Api_Products::class);
     }
 
+    public function tearDown()
+    {
+        $this->productsApiMock = null;
+    }
+
     public function testCreateBatchJson()
     {
-        $this->productsApiMock = $this->productsApiMock->setMethods(array('makeBatchId'))
+        $this->productsApiMock = $this->productsApiMock->setMethods(array('makeBatchId', 'makeProductsNotSentCollection', 'joinMailchimpSyncData'))
             ->getMock();
 
         $this->productsApiMock->expects($this->once())->method('makeBatchId')->with(0)
             ->willReturn('storeid-0_PRO_2017-05-18-14-45-54-38849500');
         $this->productsApiMock->expects($this->never())->method('buildProductDataRemoval');
+        $this->productsApiMock->expects($this->once())->method('makeProductsNotSentCollection');
+        $this->productsApiMock->expects($this->once())->method('joinMailchimpSyncData');
 
-//        $this->productsApiMock->expects($this->once())->method('makeProductsNotSentCollection')->with('dasds231231312', 0)
-//        ->willReturn(new Varien_Object());
+        $this->productsApiMock->expects($this->once())->method('makeProductsNotSentCollection')->with('dasds231231312', 0)
+        ->willReturn(new Varien_Object());
 
         $this->productsApiMock->createBatchJson('dasds231231312', 0);
     }
 
     public function testMakeProductsNotSentCollection()
     {
-        $this->productsApiMock = $this->productsApiMock->setMethods(array('sendModifiedProduct', 'generateBatchId'))
+        $this->productsApiMock = $this->productsApiMock->setMethods(
+            array(
+                'joinQtyAndBackorders',
+                'joinCategoryId',
+                'joinProductAttributes',
+                'getProductResourceCollection'
+            )
+        )
             ->getMock();
 
-//        $product->getProductUrl()
+        $dbSelectMock = $this->getMockBuilder(Varien_Db_Select::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $dbSelectMock->expects($this->once())->method('group')->with('e.entity_id');
+        $dbSelectMock->expects($this->once())->method('limit')->with(100);
+
+        $productResourceCollectionMock = $this->getMockBuilder(Mage_Catalog_Model_Resource_Product_Collection::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $productResourceCollectionMock->expects($this->exactly(2))->method('getSelect')->willReturn($dbSelectMock);
+        $productResourceCollectionMock->expects($this->once())->method('addStoreFilter');
+        $productResourceCollectionMock->expects($this->once())->method('addPriceData');
+
+        $this->productsApiMock->expects($this->once())->method('getProductResourceCollection')
+            ->willReturn($productResourceCollectionMock);
+        $this->productsApiMock->expects($this->once())->method('joinQtyAndBackorders');
+        $this->productsApiMock->expects($this->once())->method('joinCategoryId');
+        $this->productsApiMock->expects($this->once())->method('joinProductAttributes');
+        $this->productsApiMock->expects($this->once())->method('getProductResourceCollection');
 
         $collection = $this->productsApiMock->makeProductsNotSentCollection('dasds123321', 0);
 
-        $this->assertEquals("select", (string)$collection->getSelect());
+        $this->assertInstanceOf("Mage_Catalog_Model_Resource_Product_Collection", $collection);
     }
 }
