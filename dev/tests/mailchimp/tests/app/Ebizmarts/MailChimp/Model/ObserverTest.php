d2b4323a05f7caf76f833365285ce998d512ed34
centerax <pablo@ebizmarts.com>
2017-05-29 14:09:25 -0300

    tests for saveCampaignData

--- dev/tests/mailchimp/tests/app/Ebizmarts/MailChimp/Model/ObserverTest.php
+++ dev/tests/mailchimp/tests/app/Ebizmarts/MailChimp/Model/ObserverTest.php
@@ -9,7 +9,7 @@ class Ebizmarts_MailChimp_Model_ObserverTest extends PHPUnit_Framework_TestCase
 
     public function testProductAttributeUpdateIsUsingCorrectStoreId()
     {
-        /** @var \Ebizmarts_MailChimp_Helper_Data $helperMock */
+        /** @var \Ebizmarts_MailChimp_Model_Observer $modelMock */
         $modelMock = $this->getMockBuilder(Ebizmarts_MailChimp_Model_Observer::class)
             ->disableOriginalConstructor()
             ->setMethods(array('changeStoreName', 'makeHelper', 'makeApiProducts'))
@@ -31,16 +31,59 @@ class Ebizmarts_MailChimp_Model_ObserverTest extends PHPUnit_Framework_TestCase
         $eventMock->expects($this->once())->method('getProductIds')->willReturn(array(12,34));
         $eventMock->expects($this->once())->method('getStoreId')->willReturn(0);
 
-        $eventObserverMock = $this->getMockBuilder(Varien_Event_Observer::class)
-            ->disableOriginalConstructor()
-            ->setMethods(array('getEvent'))
-            ->getMock();
-        $eventObserverMock->expects($this->exactly(2))->method('getEvent')->willReturn($eventMock);
+        $eventObserverMock = $this->makeEventObserverMock($eventMock, 2);
 
         $modelMock->expects($this->never())->method('makeHelper');
 
         $modelMock->expects($this->exactly(2))->method('makeApiProducts')->willReturn($apiProductsMock);
 
         $modelMock->productAttributeUpdate($eventObserverMock);
+    }
+
+    public function testSaveCampaignDataCallsCorrectFunctions()
+    {
+        /** @var \Ebizmarts_MailChimp_Model_Observer $modelMock */
+        $modelMock = $this->getMockBuilder(Ebizmarts_MailChimp_Model_Observer::class)
+            ->disableOriginalConstructor()
+            ->setMethods(array("_getCampaignCookie", "_getLandingCookie"))
+            ->getMock();
+
+        $modelMock->expects($this->once())->method("_getCampaignCookie")->willReturn("abcd123");
+        $modelMock->expects($this->once())->method("_getLandingCookie")->willReturn("abcd");
+
+        $orderMock = $this->getMockBuilder(Mage_Sales_Model_Order::class)
+            ->setMethods(array("setMailchimpCampaignId", "getMailchimpLandingPage", "setMailchimpLandingPage"))
+            ->disableOriginalConstructor()
+            ->getMock();
+        $orderMock->expects($this->once())->method("setMailchimpCampaignId")->with("abcd123");
+        $orderMock->expects($this->once())->method("getMailchimpLandingPage")->willReturn(null);
+        $orderMock->expects($this->once())->method("setMailchimpLandingPage")->with("abcd");
+
+        $eventMock = $this->getMockBuilder(Varien_Event::class)
+            ->disableOriginalConstructor()
+            ->setMethods(array("getOrder"))
+            ->getMock();
+        $eventMock->expects($this->once())->method("getOrder")->willReturn($orderMock);
+
+        $eventObserverMock = $this->makeEventObserverMock($eventMock, 1);
+
+        $modelMock->saveCampaignData($eventObserverMock);
+    }
+
+    /**
+     * @param $eventMock
+     * @param $callCount
+     * @return PHPUnit_Framework_MockObject_MockObject
+     */
+    private function makeEventObserverMock($eventMock, $callCount)
+    {
+        $eventObserverMock = $this->getMockBuilder(Varien_Event_Observer::class)
+            ->disableOriginalConstructor()
+            ->setMethods(array('getEvent'))
+            ->getMock();
+
+        $eventObserverMock->expects($this->exactly($callCount))->method('getEvent')->willReturn($eventMock);
+
+        return $eventObserverMock;
     }
 }
