424a7cade4190ac78acd787a4d8884bda6b44c86
Santiago <santiago@ebizmarts.com>
2017-06-16 15:38:39 -0300

    Inverted parameters to create index. closes #324

--- app/code/community/Ebizmarts/MailChimp/sql/mailchimp_setup/mysql4-upgrade-1.1.6.6-1.1.6.7.php
+++ app/code/community/Ebizmarts/MailChimp/sql/mailchimp_setup/mysql4-upgrade-1.1.6.6-1.1.6.7.php
@@ -6,7 +6,7 @@ try {
     $installer->run(
         "
 ALTER TABLE `{$this->getTable('sales_flat_order')}` 
-ADD INDEX `customer_email` (`IDX_M4M_SALES_FLAT_ORDER_CUSTOMER_EMAIL`);
+ADD INDEX `IDX_M4M_SALES_FLAT_ORDER_CUSTOMER_EMAIL` (`customer_email`);
 "
     );
 } catch (Exception $e) {
