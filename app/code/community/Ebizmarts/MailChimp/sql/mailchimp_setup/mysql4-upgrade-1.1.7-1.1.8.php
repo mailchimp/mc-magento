d9278dea3ba9ab1e0a04254a5ca2db181f9ceff1
Santiago <santiago@ebizmarts.com>
2017-06-28 16:07:35 -0300

    Deadlock error fix. closes #328

--- app/code/community/Ebizmarts/MailChimp/sql/mailchimp_setup/mysql4-upgrade-1.1.7-1.1.8.php
+++ app/code/community/Ebizmarts/MailChimp/sql/mailchimp_setup/mysql4-upgrade-1.1.7-1.1.8.php
@@ -13,4 +13,15 @@ ADD INDEX `IDX_M4M_SALES_FLAT_ORDER_CUSTOMER_EMAIL` (`customer_email`);
     Mage::log($e->getMessage(), null, 'MailChimp_Errors.log', true);
 }
 
+try {
+    $installer->run(
+        "
+ALTER TABLE `{$this->getTable('mailchimp_ecommerce_sync_data')}` 
+ADD INDEX `batch_id` (`batch_id`);
+"
+    );
+} catch (Exception $e) {
+    Mage::log($e->getMessage(), null, 'MailChimp_Errors.log', true);
+}
+
 $installer->endSetup();
