b55e505aaf47866da1f91b0711e0d34a98af4344
Santiago <santiago@ebizmarts.com>
2017-12-29 17:08:42 -0300

    Add index for type column in mailchimp_ecommerce_sync_data table. resolves #584

---
+++ app/code/community/Ebizmarts/MailChimp/sql/mailchimp_setup/mysql4-upgrade-1.1.10-1.1.11.php
@@ -0,0 +1,18 @@
+<?php
+
+$installer = $this;
+
+try {
+    $installer->run(
+        "
+ALTER TABLE `{$this->getTable('mailchimp_ecommerce_sync_data')}` 
+ADD INDEX `type` (`type`);
+"
+    );
+}
+catch (Exception $e)
+{
+    Mage::log($e->getMessage(), null, 'MailChimp_Errors.log', true);
+}
+
+$installer->endSetup();
