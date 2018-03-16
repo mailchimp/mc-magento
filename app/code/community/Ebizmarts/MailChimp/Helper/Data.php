db0d7aead57285952a6c2ef18a80e445c566560c
Santiago <santiago@ebizmarts.com>
2018-03-16 17:01:27 -0300

    Added controller. closes #482

--- app/code/community/Ebizmarts/MailChimp/Helper/Data.php
+++ app/code/community/Ebizmarts/MailChimp/Helper/Data.php
@@ -47,7 +47,7 @@ class Ebizmarts_MailChimp_Helper_Data extends Mage_Core_Helper_Abstract
             $storeId = Mage::getModel('core/store')->load($code)->getId();
         } elseif ($code = Mage::getSingleton('adminhtml/config_data')->getWebsite()) {
             // website level
-            $websiteId = Mage::getModel('core/website')->load($code)->getId();
+            $websiteId = $this->getCoreWebsite()->load($code)->getId();
             $storeId = $this->getMageApp()->getWebsite($websiteId)->getDefaultStore()->getId();
         }
         $scopeArray['websiteId'] = $websiteId;
@@ -1402,6 +1402,14 @@ class Ebizmarts_MailChimp_Helper_Data extends Mage_Core_Helper_Abstract
         return Mage::getSingleton('core/resource');
     }
 
+    /**
+     * @return false|Mage_Core_Model_Abstract
+     */
+    protected function getCoreWebsite()
+    {
+        return Mage::getModel('core/website');
+    }
+
     private function getProductImageModel()
     {
         return Mage::getModel('catalog/product_image');
@@ -2480,7 +2488,7 @@ class Ebizmarts_MailChimp_Helper_Data extends Mage_Core_Helper_Abstract
             $allItemsSent = $this->allResendItemsSentPerStoreView($scopeId);
         } else {
             if ($scope == 'websites') {
-                $website = Mage::getModel('core/website')->load($scopeId);
+                $website = $this->getCoreWebsite()->load($scopeId);
                 $storeIds = $website->getStoreIds();
                 $allItemsSent = $this->allResendItemsSentPerScope($storeIds);
             } else {
@@ -2968,5 +2976,41 @@ class Ebizmarts_MailChimp_Helper_Data extends Mage_Core_Helper_Abstract
             }
         }
         return $isNewApiKeyForSameAccount;
+    }
+
+    public function resendSubscribers($scopeId, $scope = 'stores')
+    {
+        $storeIdArray = $this->getAllStoresForScope($scopeId, $scope);
+        $resource = $this->getCoreResource();
+        $connection = $resource->getConnection('core_write');
+        $tableName = $resource->getTableName('newsletter/subscriber');
+
+        foreach ($storeIdArray as $storeId) {
+            $where = array("store_id = ?" => $storeId);
+            $connection->update($tableName, array('mailchimp_sync_delta' => '0000-00-00 00:00:00'), $where);
+        }
+    }
+
+    protected function getAllStoresForScope($scopeId, $scope)
+    {
+        $storesResult = array();
+
+        switch ($scope) {
+            case 'default':
+                $stores = $this->getMageApp()->getStores();
+                foreach ($stores as $storeId => $store) {
+                    $storesResult[] = $storeId;
+                }
+                break;
+            case 'websites':
+                $website = $this->getCoreWebsite()->load($scopeId);
+                $storesResult = $website->getStoreIds();
+                break;
+            case 'stores':
+                $storesResult[] = $scopeId;
+                break;
+        }
+
+        return $storesResult;
     }
 }
---
+++ dev/tests/mailchimp/tests/app/Ebizmarts/MailChimp/controllers/Adminhtml/MailchimpControllerTest.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once BP . DS . 'app/code/community/Ebizmarts/MailChimp/controllers/Adminhtml/MailchimpController.php';
+
+class Ebizmarts_MailChimp_Adminhtml_MailchimpControllerTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Ebizmarts_MailChimp_Adminhtml_MailchimpController $mailchimpController
+     */
+    private $mailchimpController;
+
+    public function setUp()
+    {
+        Mage::app('default');
+        $this->mailchimpController = $this->getMockBuilder(Ebizmarts_MailChimp_Adminhtml_MailchimpController::class);
+    }
+
+    public function tearDown()
+    {
+        $this->mailchimpController = null;
+    }
+
+    public function testResendSubscribersAction()
+    {
+        $paramScope = 'scope';
+        $paramScopeId = 'scope_id';
+        $scope = 'stores';
+        $scopeId = 1;
+        $result = 1;
+
+        $mailchimpControllerMock = $this->mailchimpController
+            ->disableOriginalConstructor()
+            ->setMethods(array('makeHelper'))
+            ->getMock();
+
+        $helperMock = $this->getMockBuilder(Ebizmarts_MailChimp_Helper_Data::class)
+            ->disableOriginalConstructor()
+            ->setMethods(array('getMageApp', 'resendSubscribers'))
+            ->getMock();
+
+        $mageAppMock = $this->getMockBuilder(Mage_Core_Model_App::class)
+            ->disableOriginalConstructor()
+            ->setMethods(array('getRequest', 'getResponse'))
+            ->getMock();
+
+        $requestMock = $this->getMockBuilder(Mage_Core_Controller_Request_Http::class)
+            ->disableOriginalConstructor()
+            ->setMethods(array('getParam'))
+            ->getMock();
+
+        $responseMock = $this->getMockBuilder(Mage_Core_Controller_Response_Http::class)
+            ->disableOriginalConstructor()
+            ->setMethods(array('setBody'))
+            ->getMock();
+
+        $mailchimpControllerMock->expects($this->once())->method('makeHelper')->willReturn($helperMock);
+
+        $helperMock->expects($this->once())->method('getMageApp')->willReturn($mageAppMock);
+
+        $mageAppMock->expects($this->once())->method('getRequest')->willReturn($requestMock);
+
+        $requestMock->expects($this->exactly(2))->method('getParam')->withConsecutive(
+            array($paramScope),
+            array($paramScopeId))
+            ->willReturnOnConsecutiveCalls(
+                $scope,
+                $scopeId
+            );
+
+        $helperMock->expects($this->once())->method('resendSubscribers')->with($scopeId, $scope);
+        $mageAppMock->expects($this->once())->method('getResponse')->willReturn($responseMock);
+
+        $responseMock->expects($this->once())->method('setBody')->with($result);
+
+        $mailchimpControllerMock->resendSubscribersAction();
+    }
+}
