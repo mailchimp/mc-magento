55d94bcd7da884e71d0de272014501e682265bec
Santiago <santiago@ebizmarts.com>
2018-05-11 17:27:21 -0300

    Add campaignCatcher.js file only if ecommerce is enabled. closes #698

--- app/code/community/Ebizmarts/MailChimp/Model/Config.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Config.php
@@ -45,7 +45,7 @@ class Ebizmarts_MailChimp_Model_Config
     const ECOMMERCE_PRODUCT_LAST_ID     = 'mailchimp/ecommerce/product_last_id';
     const ECOMMERCE_ORDER_LAST_ID       = 'mailchimp/ecommerce/order_last_id';
     const ECOMMERCE_CART_LAST_ID        = 'mailchimp/ecommerce/cart_last_id';
-    const ECOMMERCE_PCD_LAST_ID        = 'mailchimp/ecommerce/pcd_last_id';
+    const ECOMMERCE_PCD_LAST_ID         = 'mailchimp/ecommerce/pcd_last_id';
     const ECOMMERCE_RESEND_ENABLED      = 'mailchimp/ecommerce/resend_enabled';
     const ECOMMERCE_RESEND_TURN         = 'mailchimp/ecommerce/resend_turn';
     const ECOMMERCE_CUSTOMER_AMOUNT     = 'mailchimp/ecommerce/customer_batch_amount';
