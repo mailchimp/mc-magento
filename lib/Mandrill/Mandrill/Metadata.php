dec1b3d60f617899730c02ebd8a36eeffed6f9db
juanandres@ebizmarts.com <juanandres@ebizmarts.com>
2018-08-23 16:28:51 -0300

    code sniffer #779

--- lib/Mandrill/Mandrill/Metadata.php
+++ lib/Mandrill/Mandrill/Metadata.php
@@ -26,15 +26,16 @@ class Mandrill_Metadata
      * Add a new custom metadata field to be indexed for the account.
      *
      * @param  string $name          a unique identifier for the metadata field
-     * @param  string $view_template optional Mustache template to control how the metadata is rendered in your activity log
+     * @param  string $viewTemplate optional Mustache template to control how the metadata is rendered in your activity
+     * log
      * @return struct the information saved about the new metadata field
      *     - name string the unique identifier of the metadata field to update
      *     - state string the current state of the metadata field, one of "active", "delete", or "index"
      *     - view_template string Mustache template to control how the metadata is rendered in your activity log
      */
-    public function add($name, $view_template = null)
+    public function add($name, $viewTemplate = null)
     {
-        $_params = array("name" => $name, "view_template" => $view_template);
+        $_params = array("name" => $name, "view_template" => $viewTemplate);
         return $this->master->call('metadata/add', $_params);
     }
 
@@ -42,20 +43,22 @@ class Mandrill_Metadata
      * Update an existing custom metadata field.
      *
      * @param  string $name          the unique identifier of the metadata field to update
-     * @param  string $view_template optional Mustache template to control how the metadata is rendered in your activity log
+     * @param  string $viewTemplate optional Mustache template to control how the metadata is rendered in your activity
+     * log
      * @return struct the information for the updated metadata field
      *     - name string the unique identifier of the metadata field to update
      *     - state string the current state of the metadata field, one of "active", "delete", or "index"
      *     - view_template string Mustache template to control how the metadata is rendered in your activity log
      */
-    public function update($name, $view_template)
+    public function update($name, $viewTemplate)
     {
-        $_params = array("name" => $name, "view_template" => $view_template);
+        $_params = array("name" => $name, "view_template" => $viewTemplate);
         return $this->master->call('metadata/update', $_params);
     }
 
     /**
-     * Delete an existing custom metadata field. Deletion isn't instataneous, and /metadata/list will continue to return the field until the asynchronous deletion process is complete.
+     * Delete an existing custom metadata field. Deletion isn't instataneous, and /metadata/list will continue to return
+     * the field until the asynchronous deletion process is complete.
      *
      * @param  string $name the unique identifier of the metadata field to update
      * @return struct the information for the deleted metadata field
