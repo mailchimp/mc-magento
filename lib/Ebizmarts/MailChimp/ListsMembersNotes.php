06bfbfc502f96ed47961fbe269cb267110216c59
juanandres@ebizmarts.com <juanandres@ebizmarts.com>
2018-08-23 17:27:44 -0300

    code sniffer #779

--- lib/Ebizmarts/MailChimp/ListsMembersNotes.php
+++ lib/Ebizmarts/MailChimp/ListsMembersNotes.php
@@ -15,7 +15,7 @@ class MailChimp_ListsMembersNotes extends MailChimp_Abstract
     /**
      * @param $listId           The unique id for the list.
      * @param $subscriberHash   The MD5 hash of the lowercase version of the list member’s email address.
-     * @param null                                                                                        $note The content of the note.
+     * @param null              $note The content of the note.
      * @return mixed
      * @throws MailChimp_Error
      * @throws MailChimp_HttpError
@@ -23,7 +23,8 @@ class MailChimp_ListsMembersNotes extends MailChimp_Abstract
     public function add($listId,$subscriberHash,$note=null)
     {
         $_params = array();
-        if($note) { $_params['note'] = $note;
+        if ($note) {
+            $_params['note'] = $note;
         }
         return $this->_master->call('lists/'.$listId.'/members/'.$subscriberHash, $_params, Ebizmarts_MailChimp::POST);
     }
@@ -31,13 +32,13 @@ class MailChimp_ListsMembersNotes extends MailChimp_Abstract
     /**
      * @param $listId               The unique id for the list.
      * @param $subscriberHash       The MD5 hash of the lowercase version of the list member’s email address.
-     * @param null                                                                                            $fields        A comma-separated list of fields to return. Reference parameters of sub-objects
-     *                                                                                                                       with dot notation.
-     * @param null                                                                                            $excludeFields A comma-separated list of fields to exclude. Reference parameters of sub-objects
-     *                                                                                                                       with dot notation.
-     * @param null                                                                                            $count         The number of records to return.
-     * @param null                                                                                            $offset        The number of records from a collection to skip. Iterating over large collections
-     *                                                                                                                       with this parameter can be slow.
+     * @param null        $fields        A comma-separated list of fields to return. Reference parameters of sub-objects
+     *                                                                                                with dot notation.
+     * @param null       $excludeFields A comma-separated list of fields to exclude. Reference parameters of sub-objects
+     *                                                                                                with dot notation.
+     * @param null                                                       $count         The number of records to return.
+     * @param null      $offset        The number of records from a collection to skip. Iterating over large collections
+     *                                                                                  with this parameter can be slow.
      * @return mixed
      * @throws MailChimp_Error
      * @throws MailChimp_HttpError
@@ -45,13 +46,17 @@ class MailChimp_ListsMembersNotes extends MailChimp_Abstract
     public function getAll($listId,$subscriberHash,$fields=null,$excludeFields=null,$count=null,$offset=null)
     {
         $_params = array();
-        if($fields) { $_params['fields'] = $fields;
+        if ($fields) {
+            $_params['fields'] = $fields;
         }
-        if($excludeFields) { $_params['exclude_fields'] = $excludeFields;
+        if ($excludeFields) {
+            $_params['exclude_fields'] = $excludeFields;
         }
-        if($count) { $_params['count'] = $count;
+        if ($count) {
+            $_params['count'] = $count;
         }
-        if($offset) { $_params['offset'] = $offset;
+        if ($offset) {
+            $_params['offset'] = $offset;
         }
         $url = 'lists/'.$listId.'/members/'.$subscriberHash.'/notes';
         return $this->_master->call($url, $_params, Ebizmarts_MailChimp::GET);
@@ -61,10 +66,10 @@ class MailChimp_ListsMembersNotes extends MailChimp_Abstract
      * @param $listId               The unique id for the list.
      * @param $subscriberHash       The MD5 hash of the lowercase version of the list member’s email address.
      * @param $noteId               The id for the note.
-     * @param null                                                                                            $fields        A comma-separated list of fields to return. Reference parameters of sub-objects
-     *                                                                                                                       with dot notation.
-     * @param null                                                                                            $excludeFields A comma-separated list of fields to exclude. Reference parameters of sub-objects
-     *                                                                                                                       with dot notation.
+     * @param null        $fields        A comma-separated list of fields to return. Reference parameters of sub-objects
+     *                                                                                                with dot notation.
+     * @param null       $excludeFields A comma-separated list of fields to exclude. Reference parameters of sub-objects
+     *                                                                                                with dot notation.
      * @return mixed
      * @throws MailChimp_Error
      * @throws MailChimp_HttpError
@@ -72,9 +77,11 @@ class MailChimp_ListsMembersNotes extends MailChimp_Abstract
     public function get($listId,$subscriberHash,$noteId,$fields=null,$excludeFields=null)
     {
         $_params = array();
-        if($fields) { $_params['fields'] = $fields;
+        if ($fields) {
+            $_params['fields'] = $fields;
         }
-        if($excludeFields) { $_params['exclude_fields'] = $excludeFields;
+        if ($excludeFields) {
+            $_params['exclude_fields'] = $excludeFields;
         }
         $url = 'lists/'.$listId.'/members/'.$subscriberHash.'/notes/'.$noteId;
         return $this->_master->call($url, $_params, Ebizmarts_MailChimp::GET);
@@ -84,7 +91,7 @@ class MailChimp_ListsMembersNotes extends MailChimp_Abstract
      * @param $listId               The unique id for the list.
      * @param $subscriberHash       The MD5 hash of the lowercase version of the list member’s email address.
      * @param $noteId               The id for the note.
-     * @param null                                                                                            $note The content of the note.
+     * @param null                                                                        $note The content of the note.
      * @return mixed
      * @throws MailChimp_Error
      * @throws MailChimp_HttpError
@@ -92,7 +99,8 @@ class MailChimp_ListsMembersNotes extends MailChimp_Abstract
     public function modify($listId,$subscriberHash,$noteId,$note=null)
     {
         $_params = array();
-        if($note) { $_params['note'] = $note;
+        if ($note) {
+            $_params['note'] = $note;
         }
         $url = 'lists/'.$listId.'/members/'.$subscriberHash.'/notes/'.$noteId;
         return $this->_master->call($url, $_params, Ebizmarts_MailChimp::PATCH);
