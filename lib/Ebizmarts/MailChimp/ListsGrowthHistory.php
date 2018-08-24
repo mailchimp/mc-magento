8377a3e304e901f2c7d269dd307bfe3c14b5f80b
juanandres@ebizmarts.com <juanandres@ebizmarts.com>
2018-08-24 11:41:27 -0300

    code sniffer #779

--- lib/Ebizmarts/MailChimp/ListsGrowthHistory.php
+++ lib/Ebizmarts/MailChimp/ListsGrowthHistory.php
@@ -14,12 +14,12 @@ class MailChimp_ListsGrowthHistory extends MailChimp_Abstract
 {
     /**
      * @param $listId               The unique id for the list.
-     * @param null                                            $fields        A comma-separated list of fields to return. Reference parameters of sub-objects
+     * @param null      $fields        A comma-separated list of fields to return. Reference parameters of sub-objects
      *                                                                       with dot notation.
-     * @param null                                            $excludeFields A comma-separated list of fields to exclude. Reference parameters of sub-objects
+     * @param null     $excludeFields A comma-separated list of fields to exclude. Reference parameters of sub-objects
      *                                                                       with dot notation.
-     * @param null                                            $count         The number of records to return.
-     * @param null                                            $offset        The number of records from a collection to skip. Iterating over large collections
+     * @param null                               $count         The number of records to return.
+     * @param null     $offset        The number of records from a collection to skip. Iterating over large collections
      *                                                                       with this parameter can be slow.
      * @return mixed
      * @throws MailChimp_Error
@@ -28,13 +28,17 @@ class MailChimp_ListsGrowthHistory extends MailChimp_Abstract
     public function getAll($listId,$fields=null,$excludeFields=null,$count=null,$offset=null)
     {
         $_params = array();
-        if($fields) { $_params['fields'] = $fields;
+        if ($fields) {
+            $_params['fields'] = $fields;
         }
-        if($excludeFields) { $_params['exclude_fields'] = $excludeFields;
+        if ($excludeFields) {
+            $_params['exclude_fields'] = $excludeFields;
         }
-        if($count) { $_params['count'] = $count;
+        if ($count) {
+            $_params['count'] = $count;
         }
-        if($offset) { $_params['offset'] = $offset;
+        if ($offset) {
+            $_params['offset'] = $offset;
         }
         return $this->_master->call('lists/'.$listId.'/growth-history', $_params, Ebizmarts_MailChimp::GET);
     }
@@ -42,9 +46,9 @@ class MailChimp_ListsGrowthHistory extends MailChimp_Abstract
     /**
      * @param $listId               The unique id for the list.
      * @param $month                A specific month of list growth history.
-     * @param null                                                         $fields        A comma-separated list of fields to return. Reference parameters of sub-objects
+     * @param null       $fields        A comma-separated list of fields to return. Reference parameters of sub-objects
      *                                                                                    with dot notation.
-     * @param null                                                         $excludeFields A comma-separated list of fields to exclude. Reference parameters of sub-objects
+     * @param null    $excludeFields A comma-separated list of fields to exclude. Reference parameters of sub-objects
      *                                                                                    with dot notation.
      * @return mixed
      * @throws MailChimp_Error
@@ -53,9 +57,11 @@ class MailChimp_ListsGrowthHistory extends MailChimp_Abstract
     public function get($listId,$month,$fields=null,$excludeFields=null)
     {
         $_params = array();
-        if($fields) { $_params['fields'] = $fields;
+        if ($fields) {
+            $_params['fields'] = $fields;
         }
-        if($excludeFields) { $_params['exclude_fields'] = $excludeFields;
+        if ($excludeFields) {
+            $_params['exclude_fields'] = $excludeFields;
         }
         return $this->_master->call('lists/'.$listId.'/growth-history/'.$month, $_params, Ebizmarts_MailChimp::GET);
 
