52b871e33634da5d528b134de3c4f32e3b489c0a
juanandres@ebizmarts.com <juanandres@ebizmarts.com>
2018-08-24 15:52:42 -0300

    code sniffer #779

--- app/code/community/Ebizmarts/MailChimp/sql/mailchimp_setup/mysql4-upgrade-1.1.5-1.1.5.6.php
+++ app/code/community/Ebizmarts/MailChimp/sql/mailchimp_setup/mysql4-upgrade-1.1.5-1.1.5.6.php
@@ -45,7 +45,8 @@ catch (Exception $e)
     Mage::log($e->getMessage(), null, 'MailChimp_Errors.log', true);
 }
 
-Mage::helper('mailchimp')->saveMailChimpConfig(array(array(Ebizmarts_MailChimp_Model_Config::GENERAL_MIGRATE_FROM_115, 1)), 0, 'default');
+Mage::helper('mailchimp')
+    ->saveMailChimpConfig(array(array(Ebizmarts_MailChimp_Model_Config::GENERAL_MIGRATE_FROM_115, 1)), 0, 'default');
 
 
 $installer->endSetup();
