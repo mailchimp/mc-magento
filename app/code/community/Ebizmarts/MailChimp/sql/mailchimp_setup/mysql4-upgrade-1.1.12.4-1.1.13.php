7e66354516b54a6a66a64e9e0770b8557b5feb5b
Santiago <santiago@ebizmarts.com>
2018-12-04 17:24:33 -0300

    Add created at field for groups table.

--- app/code/community/Ebizmarts/MailChimp/sql/mailchimp_setup/mysql4-upgrade-1.1.12.4-1.1.13.php
+++ app/code/community/Ebizmarts/MailChimp/sql/mailchimp_setup/mysql4-upgrade-1.1.12.4-1.1.13.php
@@ -9,6 +9,7 @@ $installer->run(
 	  `customer_id` INT(10) DEFAULT NULL,
 	  `subscriber_id` INT(10) DEFAULT NULL,
 	  `store_id` SMALLINT (5) NOT NULL DEFAULT 0,
+	  `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
 	  `updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
 	  `groupdata` TEXT(4096) NOT NULL,
 	  PRIMARY KEY  (`id`)
