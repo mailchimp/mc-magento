cdf0d3d402d78fd6040610a6a46d85c44dff6022
luciaebizmarts <luciaines@ebizmarts.com>
2019-02-28 17:19:15 -0300

    closes #887 changed the path to ECOMMERCE_XML_INCLUDE_TAXES in the IncludingTxes.php

--- app/code/community/Ebizmarts/MailChimp/Model/System/Config/Source/IncludingTaxes.php
+++ app/code/community/Ebizmarts/MailChimp/Model/System/Config/Source/IncludingTaxes.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @category   Ebizmarts
- * @package    Ebizmarts_MageMonkey
+ * @package    Ebizmarts_MailChimp
  * @author     Ebizmarts Team <info@ebizmarts.com>
  * @license    http://opensource.org/licenses/osl-3.0.php
  */
@@ -16,8 +16,8 @@ class Ebizmarts_MailChimp_Model_System_Config_Source_IncludingTaxes
     public function toOptionArray()
     {
         return array(
-            array('value' => 'yes', 'label' => Mage::helper('mailchimp')->__('Yes')),
-            array('value' => 'no', 'label' => Mage::helper('mailchimp')->__('No'))
+            array('value' => 0, 'label' => Mage::helper('mailchimp')->__('No')),
+            array('value' => 1, 'label' => Mage::helper('mailchimp')->__('Yes'))
         );
     }
 }
