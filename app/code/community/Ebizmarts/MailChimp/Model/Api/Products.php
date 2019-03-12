e9fbb88bd5ec9d829f62ed23ef6b0ef0ca4a3c5b
luciaebizmarts <luciaines@ebizmarts.com>
2019-03-12 14:58:13 -0300

    fixed error when click on button resend ecommerce data

--- app/code/community/Ebizmarts/MailChimp/Model/Api/Products.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Api/Products.php
@@ -651,7 +651,7 @@ class Ebizmarts_MailChimp_Model_Api_Products
      * @param $collection
      * @param $mailchimpStoreId
      */
-    public function joinMailchimpSyncDataWithoutWhere($collection, $mailchimpStoreId, $isForSpecialPrice)
+    public function joinMailchimpSyncDataWithoutWhere($collection, $mailchimpStoreId, $isForSpecialPrice = false)
     {
         $joinCondition = "m4m.related_id = e.entity_id and m4m.type = '%s' AND m4m.mailchimp_store_id = '%s'";
         if ($isForSpecialPrice) {
