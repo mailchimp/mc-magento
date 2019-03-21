1624bf5cab65e4c9a760f6d42f04c501aa4896de
Santiago <santiago@ebizmarts.com>
2019-03-21 16:53:06 -0300

    Update database script.

--- app/code/community/Ebizmarts/MailChimp/sql/mailchimp_setup/mysql4-upgrade-1.1.15.1-1.1.15.2.php
+++ app/code/community/Ebizmarts/MailChimp/sql/mailchimp_setup/mysql4-upgrade-1.1.15.1-1.1.15.2.php
@@ -33,16 +33,5 @@ $installer->run(
 	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 "
 );
-try {
-    $installer->run(
-        "
-ALTER TABLE `{$this->getTable('mailchimp_sync_batches')}`
-ADD INDEX `idx_status_store_id` (`status`,`store_id`);
-"
-    );
-} catch (Exception $e) {
-    Mage::log($e->getMessage(), null, 'MailChimp_Errors.log', true);
-}
-
 
 $installer->endSetup();
