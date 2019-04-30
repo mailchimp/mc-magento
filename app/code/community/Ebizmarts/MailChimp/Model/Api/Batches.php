8f7270688c722e3529a7a7f2b32a9a77f0e48cf1
luciaebizmarts <luciaines@ebizmarts.com>
2019-04-30 12:33:14 -0300

    #886 changed logs

--- app/code/community/Ebizmarts/MailChimp/Model/Api/Batches.php
+++ app/code/community/Ebizmarts/MailChimp/Model/Api/Batches.php
@@ -621,6 +621,7 @@ class Ebizmarts_MailChimp_Model_Api_Batches
         $helper = $this->getHelper();
         foreach ($files as $file) {
             $items = json_decode(file_get_contents($file));
+
             $helper->resetCountersDataSentToMailchimp();
 
             if ($items!==false) {
